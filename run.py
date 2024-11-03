bb525bdace9dad22d7f27c2b79041e1bac28f474
doppedheart <anuragagarwal530@gmail.com>
2024-11-03 12:23:43 +0000

    some changes

--- run.py
+++ run.py
@@ -3,10 +3,14 @@ from app.models.user import User
 
 app = create_app()
 with app.app_context():
-    user = User(username='admin')
-    user.set_password('password')
-    db.session.add(user)
-    db.session.commit()
+    existing_user= User.query.filter_by(username='admin').first()
+    if existing_user:
+        print("user already exists")
+    else:
+        user = User(username='admin')
+        user.set_password('password')
+        db.session.add(user)
+        db.session.commit()
 
 if __name__ == '__main__':
     app.run(debug=True)
